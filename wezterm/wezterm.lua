3e1ed2572e6dcf256c5cebbcf57b57258eb5bb40
Max <mpa.business@pm.me>
2024-10-19 02:49:20 +1100

    updated 19/10/24, added WezTerm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require("wezterm")
+local config = {}
+config.font = wezterm.font("Hack Nerd Font")
+config.font_size = 14
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.80
+return config
